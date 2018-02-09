41de3ce70877bfa6e6b7c441a20ce0773170ef97
Davis Robertson <davis@daviskr.com>
2018-02-08 18:55:32 -0600

    Adding fetching of GitHub info

--- server/routes.py
+++ server/routes.py
@@ -1,4 +1,9 @@
+import json
+from collections import defaultdict
+
 from flask import send_from_directory, render_template
+import requests
+
 from server import app
 
 
@@ -6,6 +11,42 @@ from server import app
 def splash():
     return render_template("splash.html")
 
+
+@app.route("/about/")
+def about():
+    users = ["EpicDavi", "gmac220", "TimCoding", "rebekkahkoo", "ewk298"]
+    attrs = {user: defaultdict(int) for user in users}
+
+    issues_req = requests.get("https://api.github.com/repos/TimCoding/HomeSweHome/issues")
+    issues_json = issues_req.json()
+
+    total_issues = 0
+
+    for issue in issues_json:
+        user = issue["user"]["login"]
+        attrs[user]["issues"] = attrs[user]["issues"] + 1
+        total_issues += 1
+
+    contribs_req = requests.get("https://api.github.com/repos/TimCoding/HomeSweHome/stats/contributors")
+    contribs_json = contribs_req.json()
+
+    total_commits = 0
+
+    for contributor in contribs_json:
+        user = contributor["author"]["login"]
+        total = contributor["total"]
+        attrs[user]["commits"] = attrs[user]["commits"] + total
+        total_commits += total
+
+    data = {
+        "users": attrs,
+        "totals": {
+            "issues": total_issues,
+            "commits": total_commits
+        }
+    }
+
+    return json.dumps(data)
 
 @app.route("/<path:filename>")
 def file(filename):
