6d40ef2eec1dc7478873a526c203a5e070a0cc27
rebekkahkoo <rebekkahkoo@utexas.edu>
2018-03-04 19:59:30 -0600

    problems with exporting bundle.js

--- react_app/webpack.config.js
+++ react_app/webpack.config.js
@@ -2,11 +2,26 @@ const webpack = require('webpack');
 const config = {
     entry:  __dirname + '/js/index.jsx',
     output: {
-        path: __dirname + '../server/static/',
+        path: __dirname + '/../server/static/',
         filename: 'bundle.js',
     },
     resolve: {
         extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        loaders: [
+        { 
+            test: /\.jsx?$/,         // Match both .js and .jsx files
+            exclude: /node_modules/, 
+            loader: "babel-loader", 
+            query:
+              {
+                presets:['react']
+              }
+        }
+
+
+        ]
     },
 };
 module.exports = config;
