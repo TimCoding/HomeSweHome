a4bddcf544b8f312259497a6f71ce0b225e70e41
Davis Robertson <davis@daviskr.com>
2018-03-08 18:18:04 -0600

    Reformat database code

--- server/database.py
+++ server/database.py
@@ -1,18 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
 from .config import config
 
-postgres_url = "postgresql://" + config["postgres"]["username"] + ":" + config["postgres"]["password"] + "@" + config["postgres"]["host"] + "/" + config["postgres"]["database"]
+postgres_url = "postgresql://{username}:{password}@{host}/{database}".format(**config["postgres"])
 
 engine = create_engine(postgres_url, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-Base = declarative_base()
 Base.query = db_session.query_property()
 
+
 def init_db():
-	print("URL:" + postgres_url)
-	from .models import Shelter, Park, Dog, Breed
-	Base.metadata.create_all(bind=engine)
+    print("URL:" + postgres_url)
+    from .models import Shelter, Park, Dog, Breed
+    Base.metadata.create_all(bind=engine)
--- server/models.py
+++ server/models.py
@@ -1,65 +1,72 @@
 from sqlalchemy import Table, Column, Integer, String, ForeignKey, Sequence, Float, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from .database import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 
 class Shelter(Base):
-	__tablename__ = "shelter"
-	pet_finder_id = Column(String(50), primary_key=True)
-	name = Column(String)
-	address = Column(String)
-	city = Column(String(80))
-	state = Column(String(50))
-	longitude = Column(Float)
-	latitude = Column(Float)
-	phone_number = Column(String(20))
-	description = Column(String)
-	email = Column(String)
-	zipcode = Column(Integer)
-	image_url = Column(String)
-	google_rating = Column(Float)
-	google_id = Column(String)
-	#Declaring one to many relationship here between Shelter and Dogs
-	dog = relationship("Dog", back_populates = "shelter")
+    __tablename__ = "shelter"
+    pet_finder_id = Column(String(50), primary_key=True)
+    name = Column(String)
+    address = Column(String)
+    city = Column(String(80))
+    state = Column(String(50))
+    longitude = Column(Float)
+    latitude = Column(Float)
+    phone_number = Column(String(20))
+    description = Column(String)
+    email = Column(String)
+    zipcode = Column(Integer)
+    image_url = Column(String)
+    google_rating = Column(Float)
+    google_id = Column(String)
+    # Declaring one to many relationship here between Shelter and Dogs
+    dog = relationship("Dog", back_populates="shelter")
+
 
 class Park(Base):
-	__tablename__ = "park"
-	id = Column(Integer, Sequence('park_id_seq'), primary_key=True)
-	name = Column(String)
-	address = Column(String)
-	city = Column(String(80))
-	state = Column(String(50))
-	phone_number = Column(String(20))
-	description = Column(String(255))
-	zipcode = Column(Integer)
-	image_url = Column(String)
-	google_rating = Column(Float)
-	google_id = Column(String)
+    __tablename__ = "park"
+    id = Column(Integer, Sequence('park_id_seq'), primary_key=True)
+    name = Column(String)
+    address = Column(String)
+    city = Column(String(80))
+    state = Column(String(50))
+    phone_number = Column(String(20))
+    description = Column(String)
+    zipcode = Column(Integer)
+    image_url = Column(String)
+    google_rating = Column(Float)
+    google_id = Column(String)
+
 
 class Dog(Base):
-	__tablename__ = "dog"
-	id = Column(Integer, Sequence('dog_id_seq'), primary_key=True)
-	petfinder_id = Column(Integer)
-	name = Column(String)
-	phone_number = Column(String(20))
-	address = Column(String)
-	city = Column(String(80))
-	state = Column(String(50))
-	description = Column(String(255))
-	image_url = Column(String)
-	friendly = Column(Boolean)
-	housetrained = Column(Boolean)
-	shots = Column(Boolean)
-	altered = Column(Boolean)
-	#Declaring one to many relationship here between Shelter and Dogs
-	shelter_id = Column(Integer, ForeignKey('shelter.pet_finder_id'))
-	shelter = relationship("Shelter", back_populates="dog")
-	#Declaring one to many relationship here between Dogs and Breed
-	breed = relationship("Breed", back_populates = "dog")
+    __tablename__ = "dog"
+    id = Column(Integer, Sequence('dog_id_seq'), primary_key=True)
+    petfinder_id = Column(Integer)
+    name = Column(String)
+    phone_number = Column(String(20))
+    address = Column(String)
+    city = Column(String(80))
+    state = Column(String(50))
+    description = Column(String)
+    zipcode = Column(Integer)
+    image_url = Column(String)
+    friendly = Column(Boolean)
+    housetrained = Column(Boolean)
+    shots = Column(Boolean)
+    altered = Column(Boolean)
+    # Declaring one to many relationship here between Shelter and Dogs
+    shelter_id = Column(Integer, ForeignKey('shelter.pet_finder_id'))
+    shelter = relationship("Shelter", back_populates="dog")
+    # Declaring one to many relationship here between Dogs and Breed
+    breed = relationship("Breed", back_populates="dog")
+
 
 class Breed(Base):
-	__tablename__ = "breed"
-	id = Column(Integer, Sequence('breed_id_seq'), primary_key=True)
-	breed = Column(String)
-	#Declaring one to many relationship here between Dogs and breed
-	dog = relationship("Dog", back_populates = "breed")
-	dog_id = Column(Integer, ForeignKey('dog.id'))
+    __tablename__ = "breed"
+    id = Column(Integer, Sequence('breed_id_seq'), primary_key=True)
+    breed = Column(String)
+    # Declaring one to many relationship here between Dogs and breed
+    dog = relationship("Dog", back_populates="breed")
+    dog_id = Column(Integer, ForeignKey('dog.id'))
