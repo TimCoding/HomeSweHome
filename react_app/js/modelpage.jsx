b96ff6bbcb50cc78be195c79e3109c8bff1ba814
TimCoding <timbvho@gmail.com>
2018-03-09 13:45:58 -0600

    beautify models page

--- react_app/js/modelpage.jsx
+++ react_app/js/modelpage.jsx
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import ReactDOM from 'react-dom';
+import NavBar from './navbar.jsx';
 
 export default class ModelPage extends Component {
 	render(){
 		//Use prop here to determine what type of data model page it should be
 		return (
-			<Container>
-				<Row className="models_top">
-					<Col md="8">
-						<p className="models_content">HomeSweHome</p>
-						<h3 className="models_content">Quote Here</h3>
-					</Col>
-					<Col md="4">
-						<img className="logo" src="{{ url_for('static', filename='img/homeswehomelogo.svg') }}"/>
-					</Col>
-				</Row>
-				<hr/>
-				<Row className="models_top">
-					//Insert cards here
-				</Row>
-			</Container>
+			<div>
+				<NavBar/>
+				<Container>
+					<Row className="models_top">
+						<Col md="8">
+							<p className="models_content">HomeSweHome</p>
+							<h3 className="models_content">Quote Here</h3>
+						</Col>
+						<Col md="4">
+							<img className="logo" src="/../static/img/homeswehomelogo.png"/>
+						</Col>
+					</Row>
+					<hr/>
+					<Row className="models_top">
+						//Insert cards here
+					</Row>
+				</Container>
+			</div>
 		)
 	}
 }
