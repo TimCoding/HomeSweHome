636751d3acb97587f88705d586ec647bf8077612
Davis Robertson <davis@daviskr.com>
2018-03-19 18:08:20 -0500

    Forgot to add sheltercards

--- react_app/js/shelterinfo.jsx
+++ react_app/js/shelterinfo.jsx
@@ -7,15 +7,15 @@ const ShelterInfo = (props) => {
      <Container>
        <Row>
          <Col md="auto">
-           <img className="rounded-circle" width="200" height="200" src={"/../static/img/dog1.jpg"}/>
+           <img className="rounded-circle" width="200" height="200" src={props.shelterJSON.image_urls[0]}/>
          </Col>
          <Col>
-           <h1>SHELTER DETAILS PAGE</h1>
-           <span class="text-muted">Address: </span>
+           <h1>{props.shelterJSON.name}</h1>
+           <span className="text-muted">Address: {props.shelterJSON.address}</span>
            <br/>
-           <span class="text-muted">Phone: </span>
+           <span className="text-muted">Phone: {props.shelterJSON.phone}</span>
            <br/>
-           <span class="text-muted">Yelp Rating: </span>
+           <span className="text-muted">Yelp Rating: </span>
          </Col>
        </Row>
      </Container>
