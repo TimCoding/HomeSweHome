602877663e6d9fd3deab7fc3e128a59068aefc30
rebekkahkoo <rebekkahkoo@utexas.edu>
2018-03-20 23:57:49 -0500

    fixed bug in parkinfo

--- react_app/js/parkinfo.jsx
+++ react_app/js/parkinfo.jsx
@@ -10,7 +10,7 @@ const ParkInfo = (props) => {
        <CardBody>
          <CardTitle>Info</CardTitle>
          <span>Address: {props.parkJSON.address}</span><br/>
-          <span style="padding:5px">{props.parkJSON.city}, {props.parkJSON.state} {props.parkJSON.zip}</span><br/>
+          <span>{props.parkJSON.city}, {props.parkJSON.state} {props.parkJSON.zip}</span><br/>
           <span>Phone: {props.parkJSON.phone}</span><br/>
          <span>Yelp Rating: {props.parkJSON.yelp_rating}</span><br/>
        </CardBody>
