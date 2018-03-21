2bc47b1fe185d5afed21d88df7108f3d89b6e8af
Davis Robertson <davis@daviskr.com>
2018-03-20 21:49:53 -0500

    Implement nearby routes

--- server/api.py
+++ server/api.py
@@ -3,6 +3,9 @@ from flask import jsonify, request
 from server import app
 from server.models import Dog, Shelter, Park
 from server.database import db_session
+from server.geo import order_zips
+
+NEARBY_LIMIT = 5
 
 
 @app.route("/api/")
@@ -19,6 +22,26 @@ def get_dog(dog_id):
     dog_json = dog.jsonify()
     dog_json["shelter"] = dog.shelter.jsonify()
     return jsonify(dog_json)
+
+
+@app.route("/api/dog/<int:dog_id>/nearby/")
+def get_dog_nearby(dog_id):
+    dog = db_session.query(Dog).get(dog_id)
+    if dog is None:
+        # TODO: Handle this case
+        return 404
+    zipcode = dog.zipcode
+    nearby_zips = order_zips(zipcode)[1:11]
+    parks = db_session.query(Park).filter(Park.zipcode.in_(nearby_zips)).limit(NEARBY_LIMIT).all()
+    shelters = db_session.query(Shelter).filter(Shelter.zipcode.in_(nearby_zips)).limit(NEARBY_LIMIT).all()
+    return jsonify({
+        "parks": [
+            park.jsonify() for park in parks
+        ],
+        "shelters": [
+            shelter.jsonify() for shelter in shelters
+        ]
+    })
 
 
 @app.route("/api/dogs/")
@@ -58,6 +81,26 @@ def get_shelter(shelter_id):
     return jsonify(shelter_json)
 
 
+@app.route("/api/shelter/<shelter_id>/nearby/")
+def get_shelter_nearby(shelter_id):
+    shelter = db_session.query(Shelter).get(shelter_id)
+    if shelter is None:
+        # TODO: Handle this case
+        return 404
+    zipcode = shelter.zipcode
+    nearby_zips = order_zips(zipcode)[1:11]
+    parks = db_session.query(Park).filter(Park.zipcode.in_(nearby_zips)).limit(NEARBY_LIMIT).all()
+    dogs = db_session.query(Dog).filter(Dog.zipcode.in_(nearby_zips)).limit(NEARBY_LIMIT).all()
+    return jsonify({
+        "parks": [
+            park.jsonify() for park in parks
+        ],
+        "dogs": [
+            dog.jsonify() for dog in dogs
+        ]
+    })
+
+
 @app.route("/api/shelters/")
 def get_shelters():
     start = int(request.args.get("start", 0))
@@ -83,13 +126,32 @@ def get_shelters():
 
 @app.route("/api/park/<park_id>/")
 def get_park(park_id):
-    park = db_session.query(Shelter).get(park_id)
+    park = db_session.query(Park).get(park_id)
     if park is None:
         # TODO: Handle this case
         return 404
     park_json = park.jsonify()
-    park_json["nearby_shelters"] = []
     return jsonify(park_json)
+
+
+@app.route("/api/park/<park_id>/nearby/")
+def get_park_nearby(park_id):
+    park = db_session.query(Park).get(park_id)
+    if park is None:
+        # TODO: Handle this case
+        return 404
+    zipcode = park.zipcode
+    nearby_zips = order_zips(zipcode)[1:11]
+    shelters = db_session.query(Shelter).filter(Shelter.zipcode.in_(nearby_zips)).limit(NEARBY_LIMIT).all()
+    dogs = db_session.query(Dog).filter(Dog.zipcode.in_(nearby_zips)).limit(NEARBY_LIMIT).all()
+    return jsonify({
+        "shelters": [
+            shelter.jsonify() for shelter in shelters
+        ],
+        "dogs": [
+            dog.jsonify() for dog in dogs
+        ]
+    })
 
 
 @app.route("/api/parks/")
