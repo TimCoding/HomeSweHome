52fc45404cb9f7c46f1de70c02b1e69e2eaa28e2
Emily Kwong <emilyk1217@gmail.com>
2018-03-21 17:36:11 -0500

    className in shelterCards

--- react_app/js/dogcards.jsx
+++ react_app/js/dogcards.jsx
@@ -17,7 +17,7 @@ const DogCard = (props) => {
         <div className="cards">
             <Card>
                 {/*style={{width:"10%", height:"10%"}}*/}
-                <CardImg className="dogCardImg" src={props.dogData.image_urls != "" ? props.dogData.image_urls : "/static/img/homeswehomelogo.svg"} alt="Dog image" />
+                <CardImg src={props.dogData.image_urls != "" ? props.dogData.image_urls : "/static/img/homeswehomelogo.svg"} alt="Dog image" />
                 <CardBody>
                     {/* don't know if these props work properly need data to test */}
                     <CardTitle className="dogCardTitle">{props.dogData.name}</CardTitle>
@@ -25,13 +25,13 @@ const DogCard = (props) => {
                         <ul className="fa-ul">
                             <li><span className="fa-li"><i className="fas fa-paw"></i></span><b>Breed:</b> 
                                 <p className="dogCardBreed">{dogBreed} </p>
-                                </li>
+                            </li>
                             <li><span className="fa-li"><i className="fas fa-paw"></i></span><b>Housetrained:</b>
                                 <p className="dogCardHouseTraiend"> {props.dogData.housetrained === true ? "Yes" : "No"}</p>
-                                </li>
+                            </li>
                             <li><span className="fa-li"><i className="fas fa-paw"></i></span><b>Friendly:</b> 
                                 <p className="dogCardFriendly"> {props.dogData.friendly === true ? "Yes" : "No"} </p>
-                                </li>
+                            </li>
                         </ul>
                     </CardText>
                     <a className="btn btn-primary" href={urls.dogURL(props.dogData.id)}>Meet {props.dogData.name}</a>
