31badbbe041f1cf7847efa7a3f759c7edf369c16
rebekkahkoo <rebekkahkoo@utexas.edu>
2018-03-22 18:16:49 -0500

    added about page for selenium test

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-# driver = webdriver.Chrome("/Users/rebekkahkoo/Downloads/chromedriver")
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("/Users/rebekkahkoo/Downloads/chromedriver")
+# driver = webdriver.Chrome()
 driver.get('http://homeswehome.me/')
 # driver.get('http://localhost:5000/')
 
@@ -37,5 +37,5 @@ driver.find_element_by_link_text("Visit").click()
 WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text("Meet Cappuccino"))
 driver.find_element_by_link_text("Meet Cappuccino").click()
 driver.find_element_by_class_name("navbar-brand").click()
-# driver.find_element_by_link_text("About").click()
+driver.find_element_by_link_text("About").click()
 driver.quit()
