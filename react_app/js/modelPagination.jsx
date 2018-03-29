6a84f68306c5cd43823f731f1c02830239c85524
Godfrey Macasero <godfreymacasero@gmail.com>
2018-03-29 18:46:54 -0500

    working on pagination

--- react_app/js/modelPagination.jsx
+++ react_app/js/modelPagination.jsx
@@ -8,19 +8,32 @@ export default class ModelPagination extends Component {
         this.state = {
           todos: ['a','b','c','d','e','f','g','h','i','j','k'],
           currentPage: 1,
-          todosPerPage: 3
+          todosPerPage: 1,
+          startPage: 1,
+          endPage: 11 / 1 < 5 ? 11 / 1 : 5
         };
         this.handleClick = this.handleClick.bind(this);
       }
 
     handleClick(event) {
-    this.setState({
-        currentPage: Number(event.target.id)
-    });
+        var mid = (this.startPage + this.endPage) / 2;
+        var shift = Number(event.target.id) - mid;
+        var newStart = this.state.startPage + shift;
+        var newEnd = this.state.endPage + shift;
+        alert("MID VALUE: " + mid);
+        alert("SHIFT VALUE: " + shift);
+        alert("WHAT WE CLICK ON: " + Number(event.target.id));
+        alert("START VALUE: " + Number(newStart));
+        alert("END VALUE: " + Number(newEnd));
+        this.setState({
+            currentPage: Number(event.target.id),
+            startPage: newStart,
+            endPage: newEnd
+        });
     }
 
     render() {
-    const { todos, currentPage, todosPerPage } = this.state;
+    const { todos, currentPage, todosPerPage, startPage} = this.state;
 
     // Logic for displaying current todos
     const indexOfLastTodo = currentPage * todosPerPage;
@@ -33,7 +46,11 @@ export default class ModelPagination extends Component {
 
     // Logic for displaying page numbers
     const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
+    // for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
+    //     pageNumbers.push(i);
+    // }
+
+    for (let i = this.state.startPage; i <= this.state.endPage; i++) {
         pageNumbers.push(i);
     }
 
@@ -41,11 +58,11 @@ export default class ModelPagination extends Component {
         return (
         <PaginationItem>
             <PaginationLink
-							key={number}
+				key={number}
 	            id={number}
 	            onClick={this.handleClick}>
 						{number}
-						</PaginationLink>
+			</PaginationLink>
         </PaginationItem>
         );
     });
