e69175cd5ab90d91fc79ac6039c19365889450dd
Godfrey Macasero <godfreymacasero@gmail.com>
2018-04-03 19:49:54 -0500

    updated selenium tests but needs pagination and filtering tests

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -5,20 +5,22 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.common.action_chains import ActionChains
 import unittest, time, re
 
 class GUITest(unittest.TestCase):
-    def setUp(self):
+    # def setUp(self):
         '''
         assumption: using firefox webdriver assuming machine running guitest.py
         has geckodriver installed and path is /path/to/geckodriver
         '''
-        self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Firefox()
+        driver = webdriver.Chrome("C:\webdrivers\chromedriver.exe")
         # self.driver = webdriver.Chrome("/Users/rebekkahkoo/Downloads/chromedriver")
-        self.driver.get('http://homeswehome.me/')
-        # self.driver.get('http://localhost:5000/')
+        # self.driver.get('http://homeswehome.me/')
+        driver.get('http://localhost:5000/')
 
-    def test_links(self):
+    # def test_links(self):
         '''
         splash -> dogs model page -> dog details for Tic
         -> Tic's shelter details page -> park nearby Tic details page
@@ -27,34 +29,64 @@ class GUITest(unittest.TestCase):
         -> dog nearby park (Drakie) details page -> shelter models page
         -> shelter details page -> neaby dog details page (Cappuccino) -> splash
         '''
-        driver = self.driver
+        # driver = self.driver
         driver.find_element_by_link_text("Dogs").click()
-        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text('Meet Tic'))
+        time.sleep(5)
+        element_to_hover_over = driver.find_element_by_xpath("//*[@id='content']/div/div[2]/div[2]/div[1]/div/div/img")
+        hover = ActionChains(driver).move_to_element(element_to_hover_over)
+        hover.perform()
+        # tic
         driver.find_element_by_link_text('Meet Tic').click()
         WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text("Adopt Me Please"))
         driver.find_element_by_link_text("Adopt Me Please").click()
-        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text("Visit"))
+        # town of addison
+        time.sleep(5)
+        element_to_hover_over = driver.find_element_by_xpath("//*[@id='content']/div/div[6]/div/div[1]/div/div/img")
+        hover = ActionChains(driver).move_to_element(element_to_hover_over)
+        hover.perform()
+        #addison circle park
         driver.find_element_by_link_text("Visit").click()
         driver.back()
         driver.forward()
         driver.find_element_by_link_text("Parks").click()
-        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text("Visit"))
+        time.sleep(5)
+        #bosque park
+        element_to_hover_over = driver.find_element_by_xpath("//*[@id='content']/div/div[2]/div[2]/div[2]/div/div/img")
+        hover = ActionChains(driver).move_to_element(element_to_hover_over)
+        hover.perform()
         driver.find_element_by_link_text("Visit").click()
-        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text("Visit"))
-        driver.find_element_by_link_text("Visit").click()
-        driver.back()
-        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text("Meet Drakie"))
+        time.sleep(5)
+        # drakie
+        element_to_hover_over = driver.find_element_by_xpath("//*[@id='content']/div/div[5]/div/div[3]/div/div/img")
+        hover = ActionChains(driver).move_to_element(element_to_hover_over)
+        hover.perform()
         driver.find_element_by_link_text("Meet Drakie").click()
         driver.find_element_by_link_text("Shelters").click()
-        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text("Visit"))
+        time.sleep(5)
+        # town of addison
+        element_to_hover_over = driver.find_element_by_xpath("//*[@id='content']/div/div[2]/div[2]/div[1]/div/div/img")
+        hover = ActionChains(driver).move_to_element(element_to_hover_over)
+        hover.perform()
         driver.find_element_by_link_text("Visit").click()
-        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_link_text("Meet Cappuccino"))
+        time.sleep(5)
+        element_to_hover_over = driver.find_element_by_xpath("//*[@id='content']/div/div[5]/div/div[4]/div/div/img")
+        hover = ActionChains(driver).move_to_element(element_to_hover_over)
+        hover.perform()
         driver.find_element_by_link_text("Meet Cappuccino").click()
         driver.find_element_by_class_name("navbar-brand").click()
         driver.find_element_by_link_text("About").click()
+        driver.find_element_by_xpath("//*[@id='content']/div/div[1]/nav/form/input[1]").send_keys('velcro')
+        driver.find_element_by_xpath("//*[@id='content']/div/div[1]/nav/form/input[2]").click()
+        time.sleep(10)
+        # driver.find_element_by_xpath("//*[@id='page-numbers']/li[7]/a/span[1]").click()
+        # driver.find_element_by_xpath("//*[@id='80']").click
+        element_to_hover_over = driver.find_element_by_xpath("//*[@id='content']/div/div[2]/div[2]/div/div/div/div/div/img")
+        hover = ActionChains(driver).move_to_element(element_to_hover_over)
+        hover.perform()
+        driver.find_element_by_link_text("Meet Velcro").click()
 
-    def tear_down(self):
-        self.driver.quit()
+    # def tear_down(self):
+    #     self.driver.quit()
 
-if __name__ == "__main__":
-    unittest.main()
+# if __name__ == "__main__":
+#     unittest.main()
