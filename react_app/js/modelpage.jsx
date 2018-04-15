2e2c094b6ebc05912e8ca4a5db18df1dfa9dd4d7
rebekkahkoo <rebekkahkoo@utexas.edu>
2018-04-15 18:56:20 -0500

    dogs r g8

--- react_app/js/modelpage.jsx
+++ react_app/js/modelpage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Jumbotron } from 'reactstrap';
 import ReactDOM from 'react-dom';
 import {NavBar} from './navbar.jsx';
 import * as api from './api.js';
@@ -257,7 +257,7 @@ export default class ModelPage extends Component {
 
   renderBreedsDropdown(options) {
     return (
-      <Col md="3"
+      <Col md="4"
         style={{"paddingBottom":"10px"}}>
         <Multiselect multi id="multiselect"
           closeOnSelect={false}
@@ -273,7 +273,7 @@ export default class ModelPage extends Component {
 
   renderCitiesDropdown(options) {
     return (
-      <Col md="2"
+      <Col md="3"
         style={{"paddingBottom":"10px"}}>
         <Multiselect multi
           closeOnSelect={false}
@@ -357,21 +357,32 @@ export default class ModelPage extends Component {
       const staticContent = (
         <div>
           <NavBar/>
-          <Container>
-            <Row className="models_top">
-              <Col md="8">
-                <p className="models_content">We at HomeSweHome believe that every dog deserves a home with a loving family.
-                  We also know that every dog has the potential to be a fine addition to any family. Browse through
-                  our list of adoptable dogs today and find a new family member. Our dogs come in all shapes and sizes, but each
-                  and every one of them have a huge heart made just for loving you! So stay awhile and help one of our good
-                  doggies find their Home Sweet Home.</p>
-                <h3 className="models_content">"Dogs are not our whole life, but they make our lives whole." -Roger Caras</h3>
-              </Col>
-              <Col md="4">
-                <img className="logo" src="/static/img/homeswehomelogo.svg"/>
-              </Col>
-            </Row>
-          </Container>
+            <Jumbotron style={{
+              backgroundImage:  "url(/static/img/dog1.jpg)",
+              backgroundSize: "cover",
+              minHeight:"500px",
+              maxHeight:"500px"
+            }}>
+              <Container>
+                <Row className="models_top">
+                  <Col md="8">
+                    <p className="models_content" style={{
+                      color: "white",
+                      textShadow: "0 0 25px #000, 0 0 25px #000"
+                    }}>We at HomeSweHome believe that every dog deserves a home with a loving family.
+                    We also know that every dog has the potential to be a fine addition to any family. Browse through
+                    our list of adoptable dogs today and find a new family member. Our dogs come in all shapes and sizes, but each
+                    and every one of them have a huge heart made just for loving you! So stay awhile and help one of our good
+                    doggies find their Home Sweet Home.</p>
+                    <h3 className="models_content"
+                    style={{
+                      color: "white",
+                      textShadow: "0 0 25px #000, 0 0 25px #000"
+                    }}>"Dogs are not our whole life, but they make our lives whole." -Roger Caras</h3>
+                </Col>
+                </Row>
+              </Container>
+            </Jumbotron>
         </div>
       );
 
@@ -457,23 +468,33 @@ export default class ModelPage extends Component {
         const staticContent = (
           <div>
             <NavBar/>
-            <Container>
-              <Row className="models_top">
-                <Col md="8">
-                  <p className="models_content">There is no place like the great outdoors to make memories!
-                    Luckily for you we compiled a list of all the parks we think are cool. All you have to do is focus on spending
-                    time with your favorite four-legged buddy. Here is a selection of all parks that we
-                    recommend. You will find shelters and dogs in need of an adoption near each park you look at.
-                    Please look around and plan your first puppy date today!</p>
-                  <h3 className="models_content"> "It is amazing how much love and laughter they bring into our lives and even how much closer
-                    we become with each other because of them." – John Grogan
-                  </h3>
-                </Col>
-                <Col md="4">
-                  <img className="logo" src="/static/img/homeswehomelogo.svg"/>
-                </Col>
-              </Row>
-            </Container>
+              <Jumbotron style={{
+                backgroundImage:  "url(/static/img/dog2.jpg)",
+                backgroundSize: "cover",
+                minHeight:"500px",
+                maxHeight:"600px"
+              }}>
+                <Container>
+                  <Row className="models_top">
+                    <Col md="10">
+                      <p className="models_content" style={{
+                        color: "white",
+                        textShadow: "0 0 25px #000, 0 0 25px #000"
+                      }}>There is no place like the great outdoors to make memories!
+                        Luckily for you we compiled a list of all the parks we think are cool. All you have to do is focus on spending
+                        time with your favorite four-legged buddy. Here is a selection of all parks that we
+                        recommend. You will find shelters and dogs in need of an adoption near each park you look at.
+                        Please look around and plan your first puppy date today!</p>
+                      <h3 className="models_content"
+                      style={{
+                        color: "white",
+                        textShadow: "0 0 25px #000, 0 0 25px #000"
+                      }}>"It is amazing how much love and laughter they bring into our lives and even how much closer
+                        we become with each other because of them." – John Grogan</h3>
+                    </Col>
+                  </Row>
+                </Container>
+              </Jumbotron>
           </div>
         );
 
@@ -578,19 +599,29 @@ export default class ModelPage extends Component {
           const staticContent = (
             <div>
               <NavBar/>
-              <Container>
-                <Row className="models_top">
-                  <Col md="8">
-                    <p className="models_content">Here you can browse a variety of adoption centers or animal shelters around Texas. Take a look around, your new best friend is waiting!</p>
-                    <h3 className="models_content">"Dogs are great. Why stop at one? Get two, or three, or four. Just get the whole damn
-                      shelter. Wouldn't you agree?" -Timothy Ho
-                    </h3>
-                  </Col>
-                  <Col md="4">
-                    <img className="logo" src="/static/img/homeswehomelogo.svg"/>
-                  </Col>
-                </Row>
-              </Container>
+                <Jumbotron style={{
+                  backgroundImage:  "url(/static/img/dog3.jpg)",
+                  backgroundSize: "cover",
+                  minHeight:"500px",
+                  maxHeight:"500px"
+                }}>
+                  <Container>
+                    <Row className="models_top">
+                      <Col md="8">
+                        <p className="models_content" style={{
+                          color: "white",
+                          textShadow: "0 0 25px #000, 0 0 25px #000"
+                        }}>Here you can browse a variety of adoption centers or animal shelters around Texas. Take a look around, your new best friend is waiting!</p>
+                        <h3 className="models_content"
+                        style={{
+                          color: "white",
+                          textShadow: "0 0 25px #000, 0 0 25px #000"
+                        }}>"Dogs are great. Why stop at one? Get two, or three, or four. Just get the whole damn
+                          shelter. Wouldn't you agree?" -Timothy Ho</h3>
+                      </Col>
+                    </Row>
+                  </Container>
+                </Jumbotron>
             </div>
           );
 
